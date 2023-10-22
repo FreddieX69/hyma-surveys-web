7c40b5ec6b843a8b3acb3e6aac74b5457d480b76
Fredy Alexander Xalín <65146506+FreddieX69@users.noreply.github.com>
2023-10-21 22:28:15 -0600

    fix: index to responses

--- database/migrations/2023_10_17_010746_create_patient_responses_table.php
+++ database/migrations/2023_10_17_010746_create_patient_responses_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('patient_form_id');
             $table->unsignedBigInteger('field_id');
             $table->unsignedBigInteger('field_answer_id')->nullable();
-            $table->text('answer')->index()->nullable();
+            $table->text('answer')->fulltext()->nullable();
             $table->foreign('patient_form_id')->references('id')->on('patient_forms');
             $table->foreign('field_id')->references('id')->on('fields');
             $table->foreign('field_answer_id')->references('id')->on('field_answers');
