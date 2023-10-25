f72cd55fc8183f6ea873a7442da4aeb4f5b1bff3
Fredy Alexander Xalín <65146506+FreddieX69@users.noreply.github.com>
2023-10-25 00:26:15 -0600

    fix: dashboard

--- resources/views/livewire/app/dashboard/dashboard.blade.php
+++ resources/views/livewire/app/dashboard/dashboard.blade.php
@@ -85,7 +85,7 @@
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Último paciente</h6>
-                                        <h6 class="font-extrabold mb-0">{{ \App\Models\Patient::orderBy('id', 'DESC')->first()->name }}</h6>
+                                        <h6 class="font-extrabold mb-0">{{ \App\Models\Patient::orderBy('id', 'DESC')->first()->name ?? '' }}</h6>
                                     </div>
                                 </div>
                             </div>
